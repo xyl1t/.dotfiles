dd5eec4c911cbe30490ff9e1d24fc3bf1e548d9f
Marat Isaw <marat.isaw23@gmail.com>
2022-02-13 23:18:50 +0100

    nvim: fix lightspeed incorrect mapping on normal mode

--- nvim/.config/nvim/lua/marat/lightspeed.lua
+++ nvim/.config/nvim/lua/marat/lightspeed.lua
@@ -6,8 +6,10 @@
 
 vim.g.lightspeed_no_default_keymaps = 1;
 
-vim.cmd[[map s <Plug>Lightspeed_s]]
-vim.cmd[[map S <Plug>Lightspeed_S]]
-vim.cmd[[map x <Plug>Lightspeed_x]]
-vim.cmd[[map X <Plug>Lightspeed_X]]
+vim.cmd[[nmap s <Plug>Lightspeed_s]]
+vim.cmd[[nmap S <Plug>Lightspeed_S]]
+vim.cmd[[xmap s <Plug>Lightspeed_s]]
+vim.cmd[[xmap S <Plug>Lightspeed_S]]
+vim.cmd[[omap x <Plug>Lightspeed_x]]
+vim.cmd[[omap X <Plug>Lightspeed_X]]
 

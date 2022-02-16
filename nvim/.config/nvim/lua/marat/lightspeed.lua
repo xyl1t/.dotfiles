26bb95a4dbb2645da088cdcd2e1a7b81e80acc8b
Marat Isaw <marat.isaw23@gmail.com>
2022-02-16 09:38:29 +0100

    nvim(lightspeed): use bidirectional search
    More at: https://www.reddit.com/r/neovim/comments/ssc5ys/lightspeed_bidirectional_search

--- nvim/.config/nvim/lua/marat/lightspeed.lua
+++ nvim/.config/nvim/lua/marat/lightspeed.lua
@@ -6,10 +6,7 @@
 
 vim.g.lightspeed_no_default_keymaps = 1;
 
-vim.cmd[[nmap s <Plug>Lightspeed_s]]
-vim.cmd[[nmap S <Plug>Lightspeed_S]]
-vim.cmd[[xmap s <Plug>Lightspeed_s]]
-vim.cmd[[xmap S <Plug>Lightspeed_S]]
-vim.cmd[[omap x <Plug>Lightspeed_x]]
-vim.cmd[[omap X <Plug>Lightspeed_X]]
+vim.cmd[[nmap s <Plug>Lightspeed_omni_s]]
+vim.cmd[[xmap s <Plug>Lightspeed_omni_s]]
+vim.cmd[[omap x <Plug>Lightspeed_omni_s]]
 

149764cc64650902f188dd3860ca3341553e9cb2
Marat Isaw <marat.isaw23@gmail.com>
2022-03-13 22:42:04 +0100

    nvim: keymap ctrl-s to lightspeed

--- nvim/.config/nvim/lua/marat/lightspeed.lua
+++ nvim/.config/nvim/lua/marat/lightspeed.lua
@@ -6,7 +6,11 @@
 
 vim.g.lightspeed_no_default_keymaps = 1;
 
-vim.cmd[[nmap s <Plug>Lightspeed_omni_s]]
-vim.cmd[[xmap s <Plug>Lightspeed_omni_s]]
+vim.cmd[[nmap <C-s> <Plug>Lightspeed_omni_s]]
+vim.cmd[[xmap <C-s> <Plug>Lightspeed_omni_s]]
 vim.cmd[[omap x <Plug>Lightspeed_omni_s]]
 
+require'lightspeed'.setup {
+	safe_labels = { "f", "n", "u", "t", "/", "F", "L", "N", "H", "G", "M", "U", "T", "?", "Z"},
+	exit_after_idle_msecs = { labeled = 1000, unlabeled = 1000 },
+}

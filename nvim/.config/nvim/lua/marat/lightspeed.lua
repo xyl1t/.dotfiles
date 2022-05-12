d6fa01e9d047e439d3720f25015d27810d0354f6
xyl1t <marat.isaw23@gmail.com>
2022-05-12 18:58:11 +0200

    nvim(lightspeed): stopped working :(

--- nvim/.config/nvim/lua/marat/lightspeed.lua
+++ nvim/.config/nvim/lua/marat/lightspeed.lua
@@ -1,16 +1,16 @@
--- vim.cmd[[
--- 	noremap ; ;
--- 	noremap , ,
--- ]]
+-- -- vim.cmd[[
+-- -- 	noremap ; ;
+-- -- 	noremap , ,
+-- -- ]]
+-- --
 --
-
-vim.g.lightspeed_no_default_keymaps = 1;
-
-vim.cmd[[nmap <C-s> <Plug>Lightspeed_omni_s]]
-vim.cmd[[xmap <C-s> <Plug>Lightspeed_omni_s]]
-vim.cmd[[omap x <Plug>Lightspeed_omni_s]]
-
-require'lightspeed'.setup {
-	safe_labels = { "f", "n", "u", "t", "/", "F", "L", "N", "H", "G", "M", "U", "T", "?", "Z"},
-	exit_after_idle_msecs = { labeled = 1000, unlabeled = 1000 },
-}
+-- vim.g.lightspeed_no_default_keymaps = 1;
+--
+-- vim.cmd[[nmap <C-s> <Plug>Lightspeed_omni_s]]
+-- vim.cmd[[xmap <C-s> <Plug>Lightspeed_omni_s]]
+-- vim.cmd[[omap x <Plug>Lightspeed_omni_s]]
+--
+-- require'lightspeed'.setup {
+-- 	safe_labels = { "f", "n", "u", "t", "/", "F", "L", "N", "H", "G", "M", "U", "T", "?", "Z"},
+-- 	exit_after_idle_msecs = { labeled = 1000, unlabeled = 1000 },
+-- }

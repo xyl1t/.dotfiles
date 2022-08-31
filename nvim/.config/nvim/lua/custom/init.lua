da0beeb9fbb9b81ce74822ca88f8256625931a97
xyl1t <marat.isaw23@gmail.com>
2022-08-31 15:29:42 +0200

    nvim: fix autocmd not loading

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -9,19 +9,29 @@ vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 16
 vim.opt.list = true
 vim.opt.listchars = {tab = '  ', extends = '>', precedes = '<', nbsp = '␣', trail = '·'}
-vim.opt.colorcolumn = 80
+
+-- make cursor always white
+vim.cmd[[
+highlight Cursor guifg=white guibg=black
+highlight iCursor guifg=white guibg=black
+set guicursor=n-v-c:block-Cursor
+set guicursor+=i:ver100-Cursor
+set guicursor+=n-v-c:blinkon0
+set guicursor+=i:blinkwait10
+]]
 
 -- highlight yanked text for 200ms
 vim.cmd[[
 augroup highlight_yank
     autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
+    au TextYankPost * silent! lua vim.highlight.on_yank {higroup="Visual", timeout=150}
 augroup END
 ]]
 
 -- disable incorrect indentation for swift files
 vim.cmd[[autocmd FileType swift set nosi nocindent ai]]
 
+-- disable adding comment on new line
 vim.cmd[[autocmd BufEnter * setlocal formatoptions-=o]]
 
 -- Removes trailing spaces
@@ -35,9 +45,4 @@ command! TrimWhitespace call TrimWhitespace()
 map <F2> :TrimWhitespace<CR>
 map! <F2> :TrimWhiteSpace<CR>
 ]]
-
--- vim.cmd [[
--- highlight OverLength ctermbg=red ctermfg=white guibg=#592929
--- match OverLength /\%81v./
--- ]]
 
